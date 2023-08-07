ce42258673f2f3dbbdce59a16ab39c34852af684
SouksavaDev <souksavath.dev@gmail.com>
2023-08-07 22:25:06 +0700

    status

--- routes/pay_massage.js
+++ routes/pay_massage.js
@@ -1,4 +1,4 @@
-const { SavePayMassage, billno} = require('../controllers/paymassage/pay_controller');
+const { SavePayMassage, billno, UpdateStatus } = require('../controllers/paymassage/pay_controller');
 const { checkToken } = require('../controllers/auth/verify_token_validate');
 var express = require('express');
 var router = express.Router();
@@ -7,5 +7,6 @@ router.get('/', function (req, res, next) {
   res.json('Pay massage');
 });
 router.get("/bill/:shopID", checkToken, billno);
+router.post("/updateStatus", checkToken, UpdateStatus);
 router.post("/paymassage", checkToken, SavePayMassage);
 module.exports = router;
